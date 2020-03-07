848a15637650f2b8d3513895341694caae273914
Victor Gil <baskeboler@gmail.com>
2020-03-06 23:07:47 -0300

    cards

--- src/poker_app/service.clj
+++ src/poker_app/service.clj
@@ -3,9 +3,12 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
             [ring.util.response :as ring-resp]
-            [hiccup.page :as hiccup]            
-            [clojure.java.io :as io]))
-
+            [hiccup.page :as hiccup]
+            [clojure.java.io :as io]
+            [ring.middleware.session :as sess]
+            [ring.middleware.session.cookie :as cookie]
+            [poker-app.service :as service]
+            [io.pedestal.http :as server]))
 (def card-templates
   {:heart    "hearts.svg"
    :diamonds "diamonds.svg"
@@ -25,17 +28,32 @@
 
 (defn all-cards-page
   [request]
-  (ring-resp/response
-    (hiccup/html5
-     [:body
-      [:h1 "poker cards"]
-      [:div.poker-cards
-        (for [kind  [:heart :clover :diamonds :spades]
-              value (shuffle
-                     (concat (range 2 11) ["J" "K" "Q" "A"]))]
-          [:div.poker-card {:style "display: inline-block; margin: 0.2em; position: relative; height: 200px; width: auto;"}
-           [:img {:src   (str "/poker-cards/" (name kind) "/" value)
-                  :style "height: 100%;"}]])]]))) 
+  (let [cards
+        (if-not (-> request
+                    :session
+                    :cards
+                    first
+                    nil?)
+          (-> request :session :cards)
+          (->>
+           (for [kind  [:heart :clover :diamonds :spades]
+                 value (concat (range 2 11) ["J" "K" "Q" "A"])]
+             [kind value])
+           shuffle))]
+
+    (->
+     (ring-resp/response
+      (hiccup/html5
+       [:body
+        [:h1 "poker cards"]
+        [:div.poker-cards
+         (->> cards
+              (map (fn [[kind value]]
+                     [:div.poker-card {:style "display: inline-block; margin: 0.2em; position: relative; height: 200px; width: auto;"}
+                      [:img {:src   (str "/poker-cards/" (name kind) "/" value)
+                             :style "height: 100%;"}]])))]]))
+     (assoc :session  {:cards cards}))))
+    ;; request))
 (defn about-page
   [request]
   (ring-resp/response (format "Clojure %s - served from %s"
@@ -46,24 +64,37 @@
   [request]
   (ring-resp/response "Hello World!"))
 
-
 (defn card-request
   [request]
   (let [card-kind  (-> request :path-params :card-kind)
         card-value (-> request :path-params :card-value)]
     (-> (get-card (keyword card-kind) card-value)
         ring-resp/response
-        (ring-resp/content-type "image/svg+xml"))))
+        (ring-resp/content-type "image/svg+xml")
+        (assoc-in [:session :cards] (-> request :session :cards)))))
   ;; Defines "/" and "/about" routes with their associated :get handlers.
   ;; The interceptors defined after the verb map (e.g., {:get home-page}
   ;; apply to / and its children (/about).
-  
+
+
+(defn get-next-card
+  [request]
+  ;; (let [{cards :cards} (-> request :session)]
+  (if-let [[kind value] (-> request :session :cards first)]
+    (->
+      (ring-resp/response
+        (get-card kind value))
+     (ring-resp/content-type "image/svg+xml")
+     (assoc-in [:session :cards] (-> request :session :cards rest)))
+    (all-cards-page request)))
+
 (def common-interceptors [(body-params/body-params) http/html-body])
 
 ;; Tabular routes
 (def routes #{["/" :get (conj common-interceptors `home-page)]
               ["/about" :get (conj common-interceptors `about-page)]
               ["/poker-cards" :get (conj common-interceptors `all-cards-page)]
+              ["/poker-cards-next" :get (conj common-interceptors `get-next-card)]
               ["/poker-cards/:card-kind/:card-value" :get (conj common-interceptors `card-request)]})
 
 ;; Map-based routes
@@ -87,7 +118,9 @@
               ;; default interceptors will be ignored.
               ;; ::http/interceptors []
               ::http/routes routes
-
+              :session {:store (cookie/cookie-store {:key (bytes
+                                                           (byte-array
+                                                            (repeatedly 16 #(byte (rand-int 128)))))})}
               ;; Uncomment next line to enable CORS support, add
               ;; string(s) specifying scheme, host and port for
               ;; allowed source(s):
@@ -122,4 +155,4 @@
                                         ;; Alternatively, You can specify you're own Jetty HTTPConfiguration
                                         ;; via the `:io.pedestal.http.jetty/http-configuration` container option.
                                         ;:io.pedestal.http.jetty/http-configuration (org.eclipse.jetty.server.HttpConfiguration.)
-                                        
+
